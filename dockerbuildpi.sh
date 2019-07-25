324cd30b61820db87934c532661c9e8eab7dfc12
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2019-07-26 01:28:06 +0900

    update for pi_mgr

--- dockerbuildpi.sh
+++ dockerbuildpi.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cname=`cat ./cname`
 docker build ./ -t $cname 
-docker run -t --init --name $cname --net=host -v `pwd`:/work/ -p 5510:5510/tcp -p 5511:5511/tcp --restart=always $cname 
+
+docker container stop $cname
+docker container rm $cname
+
+docker run -d --init --name $cname --net=host -v `pwd`:/work/ -p 5510:5510/tcp -p 5511:5511/tcp --restart=always $cname 
