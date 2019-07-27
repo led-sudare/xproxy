deac6e94e934044c7e53bcbc9c85ff424ccdee31
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2019-07-27 15:24:57 +0900

    add echo

--- dockerbuildpi.sh
+++ dockerbuildpi.sh
@@ -2,7 +2,7 @@
 cname=`cat ./cname`
 docker build ./ -t $cname 
 
-docker container stop $cname
-docker container rm $cname
+echo "stopping.. " && docker container stop $cname
+echo "removing.. " && docker container rm $cname
 
-docker run -d --init --name $cname --net=host -v `pwd`:/work/ -p 5510:5510/tcp -p 5511:5511/tcp --restart=always $cname 
+echo "run and stating.. " && docker run -d --init --name $cname --net=host -v `pwd`:/work/ -p 5510:5510/tcp -p 5511:5511/tcp --restart=always $cname 
